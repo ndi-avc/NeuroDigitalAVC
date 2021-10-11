3425a6e599f6153b8ff307f1172882bf671359a8
Heli Ribeiro <heliribeiro2@gmail.com>
2021-10-11 20:54:09 -0300

    Refatorando o codigo da lista para FlatList

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -18,88 +18,82 @@ const B = ({ children }) => (
   <Text style={{ fontWeight: "bold" }}>{children}</Text>
 );
 
-const SECTIONS = [
+const DATA = [
   {
-    title: "Nosso time",
-    horizontal: true,
-    data: [
-      {
-        key: "1",
-        text: "Nickolas Silva",
-        cargo: "Idealizador e PO",
-        uri: require("../../../img/picture-nickolas.png"),
-      },
+    key: "1",
+    text: "Nickolas Silva",
+    cargo: "Idealizador e PO",
+    uri: require("../../../img/picture-nickolas.png"),
+  },
 
-      {
-        key: "2",
-        text: "Heli Ribeiro",
-        cargo: "Tech Lead",
-        uri: require("../../../img/picture-heli.png"),
-      },
+  {
+    key: "2",
+    text: "Heli Ribeiro",
+    cargo: "Tech Lead",
+    uri: require("../../../img/picture-heli.png"),
+  },
 
-      {
-        key: "3",
-        text: "Robson Gomes",
-        cargo: "Designer UX/UI",
-        uri: require("../../../img/picture-robson.png"),
-      },
+  {
+    key: "3",
+    text: "Robson Gomes",
+    cargo: "Designer UX/UI",
+    uri: require("../../../img/picture-robson.png"),
+  },
 
-      {
-        key: "4",
-        text: "Luan Moraes",
-        cargo: "Programador Mobile",
-        uri: require("../../../img/picture-luan.png"),
-      },
+  {
+    key: "4",
+    text: "Luan Moraes",
+    cargo: "Programador Mobile",
+    uri: require("../../../img/picture-luan.png"),
+  },
 
-      {
-        key: "5",
-        text: "Alanna Paiva",
-        cargo: "Programadora Mobile",
-        uri: "",
-      },
+  {
+    key: "5",
+    text: "Alanna Paiva",
+    cargo: "Programadora Mobile",
+    uri: "",
+  },
 
-      {
-        key: "6",
-        text: "Rafael Mendes",
-        cargo: "Programador Mobile",
-        uri: "",
-      },
+  {
+    key: "6",
+    text: "Rafael Mendes",
+    cargo: "Programador Mobile",
+    uri: "",
+  },
 
-      {
-        key: "7",
-        text: "Gabriel Santos",
-        cargo: "Conteúdo teórico",
-        uri: require("../../../img/picture-gabriel.png"),
-      },
+  {
+    key: "7",
+    text: "Gabriel Santos",
+    cargo: "Conteúdo teórico",
+    uri: require("../../../img/picture-gabriel.png"),
+  },
 
-      {
-        key: "8",
-        text: "Cristiane Torres",
-        cargo: "Conteúdo teórico",
-        uri: require("../../../img/picture-cristiane.png"),
-      },
+  {
+    key: "8",
+    text: "Cristiane Torres",
+    cargo: "Conteúdo teórico",
+    uri: require("../../../img/picture-cristiane.png"),
+  },
 
-      {
-        key: "9",
-        text: "Rebeca Freitas",
-        cargo: "Conteúdo teórico",
-        uri: require("../../../img/picture-rebeca.png"),
-      },
+  {
+    key: "9",
+    text: "Rebeca Freitas",
+    cargo: "Conteúdo teórico",
+    uri: require("../../../img/picture-rebeca.png"),
+  },
 
-      {
-        key: "10",
-        text: "Erislândia Paiva",
-        cargo: "Conteúdo teórico",
-        uri: require("../../../img/picture-eris.png"),
-      },
+  {
+    key: "10",
+    text: "Erislândia Paiva",
+    cargo: "Conteúdo teórico",
+    uri: require("../../../img/picture-eris.png"),
+  },
 
-      {
-        key: "11",
-        text: "Luísa Silva",
-        cargo: "Conteúdo teórico",
-        uri: require("../../../img/picture-luiza.png"),
-      },
-    ],
+  {
+    key: "11",
+    text: "Luísa Silva",
+    cargo: "Conteúdo teórico",
+    uri: require("../../../img/picture-luiza.png"),
   },
 ];
 
@@ -122,12 +116,12 @@ const ListItem = ({ item }) => (
 function About() {
   return (
     <View style={styles.container}>
-      <TouchableOpacity activeOpacity={0.9}>
+      {/* <TouchableOpacity activeOpacity={0.9}>
         <Image
           source={require("../../../img/chevron-left.png")}
           style={styles.buttonImageIconStyle}
         />
-      </TouchableOpacity>
+      </TouchableOpacity> */}
 
       <StatusBar color="#fff" />
       <SafeAreaView style={{ flex: 1 }}>
@@ -155,30 +149,17 @@ function About() {
             </Text>
           </>
 
-          <SectionList
-            contentContainerStyle={{ paddingHorizontal: 40 }}
-            stickySectionHeadersEnabled={false}
-            sections={SECTIONS}
-            renderSectionHeader={({ section }) => (
-              <>
-                <Text style={styles.sectionHeader}>{section.title}</Text>
-                {section.horizontal ? (
-                  <FlatList
-                    horizontal
-                    data={section.data}
-                    renderItem={({ item }) => <ListItem item={item} />}
-                    showsHorizontalScrollIndicator={false}
-                  />
-                ) : null}
-              </>
-            )}
-            renderItem={({ item, section }) => {
-              if (section.horizontal) {
-                return null;
-              }
-              return <ListItem item={item} />;
-            }}
-          />
+          <View style={{ paddingHorizontal: 40 }}>
+            <Text style={styles.sectionHeader}>Nosso time</Text>
+            <FlatList
+              data={DATA}
+              renderItem={({ item }) => (
+                <ListItem item={item} style={styles.item} />
+              )}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            />
+          </View>
         </ScrollView>
       </SafeAreaView>
     </View>
